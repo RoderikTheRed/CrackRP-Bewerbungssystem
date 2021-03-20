5209bbf65d7cffe8de23690bafcabed56da034d7
RoderikTheRed <66172264+RoderikTheRed@users.noreply.github.com>
2021-03-20 13:58:56 +0100

    Update open.php

--- open.php
+++ open.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 if(!isset($_SESSION["username"])) {
-  header("Location: index..php");
+  header("Location: admin.php");
   exit;
 }
- ?>
+?>
 <!DOCTYPE html>
 <html lang="de" dir="ltr">
   <head>
@@ -13,24 +13,37 @@ if(!isset($_SESSION["username"])) {
   </head>
   <body>
     <?php
-    if(!empty($_GET["discord"])) {
+    if(!empty($_GET["ingame"])) {
       require("mysql.php");
-      $stmt = $mysql->prepare("SELECT * FROM bewerbungen WHERE discord = :discord");
-      $stmt->bindParam(":discord", $_GET["discord"]);
+      $stmt = $mysql->prepare("SELECT * FROM bewerbungen WHERE INGAME = :ingame");
+      $stmt->bindParam(":ingame", $_GET["ingame"]);
       $stmt->execute();
       $row = $stmt->fetch();
-      ?>
-      <form class="box" action="open.php" method="post">
+    }
+    if(isset($_POST["accept"])) {
+      require("mysql.php");
+      $stmt = $mysql->prepare("UPDATE bewerbungen SET STATUS = 'Akzeptiert'");
+      $stmt->execute();
+      header("Location: adminpanel.php?sucess=Akzeptiert");
+    }
+    if(isset($_POST["deny"])) {
+      $stmt = $mysql->prepare("UPDATE bewerbungen SET STATUS = 'Abgelehnt'");
+      $stmt->execute();
+      header("Location: adminpanel.php?sucess=Abgelehnt");
+    }
+    if(isset($_POST["close"])) {
+      header("Location: adminpanel.php");
+    }
+     ?>
+      <form class="box" action="open.php?<?php echo $row["INGAME"] ?>" method="post">
         <h2>Bewerbung:</h2>
-        <?php echo $row["DISCORD"] ?>
         <p>Discord: <?php echo $row["DISCORD"] ?></p>
         <p>Ingame Name: <?php echo $row["INGAME"] ?></p>
+        <p>OOC: <?php echo $row["OOC"] ?></p>
+        <p>IC: <?php echo $row["IC"] ?></p>
+        <input type="submit" name="accept" value="Akzeptieren">
+        <input type="submit" name="deny" value="Ablehnen">
+        <input type="submit" name="close" value="Schließen">
       </form>
-      <?php
-    } else {
-      header("Location: index.php");
-    }
-
-     ?>
   </body>
 </html>
